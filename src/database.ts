0c1e9a38da2633035c811afde3b326dd265f577f
Dablakbandit <ashl.e.thew@gmail.com>
2024-03-24 04:32:34 +1300

    Fix mysql connection

--- src/database.ts
+++ src/database.ts
@@ -2,8 +2,7 @@ import mysql, { QueryOptions, queryCallback } from 'mysql';
 import { config } from './config';
 const { mysqlHost, mysqlPort, mysqlDatabase, mysqlUsername, mysqlPassword } = config;
 
-// Create a MySQL connection
-export const connection = mysql.createConnection({
+export const connection = mysql.createPool({
     host: mysqlHost,
     port: parseInt(mysqlPort),
     user: mysqlUsername,
@@ -15,20 +14,6 @@ export const connection = mysql.createConnection({
 
 export const query = (options: string | QueryOptions, values: any, callback?: queryCallback) =>{
     return connection.query(options, values, (error, results)=>{
-        if(error && error.code === 'ECONNRESET'){
-            console.log('MySQL connection lost. Reconnecting...');
-            connection.connect((connectError) => {
-                if(connectError){
-                    console.error('Error reconnecting to MySQL:', connectError);
-                    if (callback) {
-                        callback(error, results);
-                    }
-                } else {
-                    query(options, values, callback);
-                }
-            });
-            return;
-        }
         if (callback) {
             callback(error, results);
         }
@@ -36,7 +21,7 @@ export const query = (options: string | QueryOptions, values: any, callback?: qu
 }
 
 // Test the connection
-connection.connect((error) => {
+connection.query('SELECT 1', (error, results) => {
     if (error) {
         console.error('Error connecting to MySQL:', error);
     } else {
@@ -44,16 +29,10 @@ connection.connect((error) => {
     }
 });
 
-// Enable auto-reconnect
 connection.on('error', (error) => {
     console.log("CODE: " + error.code);
     if (error.code === 'ECONNRESET') {
         console.log('MySQL connection lost. Reconnecting...');
-        connection.connect((connectError) => {
-            if(connectError){
-                console.error('Error reconnecting to MySQL:', connectError);
-            }
-        });
     } else {
         throw error;
     }
