f33908e3862dbaeaff18cff772ad73135101e29e
Dablakbandit <ashl.e.thew@gmail.com>
2024-08-05 00:22:07 +1200

    Add pagination to top commands

--- src/commands/skills/top.ts
+++ src/commands/skills/top.ts
@@ -1,4 +1,6 @@
-import { SlashCommandBuilder, PermissionFlagsBits, ChatInputCommandInteraction, CacheType, ThreadChannel, ForumChannel, ThreadOnlyChannel, EmbedBuilder } from "discord.js";
+import { SlashCommandBuilder, PermissionFlagsBits, ChatInputCommandInteraction, CacheType, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, ComponentType, MessageComponentInteraction, InteractionEditReplyOptions } from "discord.js";
+import { ButtonInteraction } from 'discord.js';
+
 import { queryCall } from "../../database.js";
 import {skills, emojis, getLevelIndex, formatNumber, capitalize} from "../../util/skills.js";
 
@@ -88,62 +90,83 @@ function getTop(type: string, perPage: number, offset: number) {
 
 async function handleTotal(interaction: ChatInputCommandInteraction<CacheType>) {
 	await interaction.deferReply();
-	const page = Math.max(0, interaction.options.getInteger('page') || 1);
+	const page = Math.max(1, interaction.options.getInteger('page') || 1);
 	const perPage = 12;
-	const offset = (page - 1) * perPage;
 
-	queryCall(getTop("total", perPage, offset), [], (error, results) => {
-		if(error || !results.length){
-			console.error(error);
-			return interaction.editReply({ content: 'No results found.'});
-		}
-		handleResult(interaction, "total", results, offset);
-	});
+	const queryCallback = async (interaction: ChatInputCommandInteraction<CacheType> | ButtonInteraction, page: number) => {
+		const offset = (page - 1) * perPage;
+
+		queryCall(getTop("total", perPage, offset), [], (error, results) => {
+			if(error || !results.length){
+				console.error(error);
+				if((interaction as ButtonInteraction<CacheType>).update){
+					return (interaction as ButtonInteraction<CacheType>).update({ content: 'No results found.'});
+				}
+				return interaction.editReply({ content: 'No results found.'});
+			}
+			handleResult(interaction, "total", results, offset, page, queryCallback);
+		});
+	}
+	queryCallback(interaction, page);
 }
 
 
 async function handleLevel(interaction: ChatInputCommandInteraction<CacheType>) {
 	await interaction.deferReply();
-	const page = Math.max(0, interaction.options.getInteger('page') || 1);
+	const page = Math.max(1, interaction.options.getInteger('page') || 1);
 	const perPage = 12;
-	const offset = (page - 1) * perPage;
-	
-	queryCall(getTop("level", perPage, offset), [], (error, results) => {
-		if(error || !results.length){
-			console.error(error);
-			return interaction.editReply({ content: 'No results found.'});
-		}
-		handleResult(interaction, "level", results, offset);
-	});
+
+	const queryCallback = async (interaction: ChatInputCommandInteraction<CacheType> | ButtonInteraction, page: number) => {
+
+		const offset = (page - 1) * perPage;
+		
+		queryCall(getTop("level", perPage, offset), [], (error, results) => {
+			if(error || !results.length){
+				console.error(error);
+				if((interaction as ButtonInteraction<CacheType>).update){
+					return (interaction as ButtonInteraction<CacheType>).update({ content: 'No results found.'});
+				}
+				return interaction.editReply({ content: 'No results found.'});
+			}
+			handleResult(interaction, "level", results, offset, page, queryCallback);
+		});
+	}
+	queryCallback(interaction, page);
 }
 
 async function handleSkill(interaction: ChatInputCommandInteraction<CacheType>, skill: string) {
 	await interaction.deferReply();
-
-	const page = Math.max(0, interaction.options.getInteger('page') || 1);
+	const page = Math.max(1, interaction.options.getInteger('page') || 1);
 	const perPage = 12;
-	const offset = (page - 1) * perPage;
 
-	const selectSql = `SELECT cache.hiscores_normal_exp.${skill}, \
-						minescape.character_ids.uuid, \
-						minescape.uuids.username FROM cache.hiscores_normal_exp, \
-						minescape.character_ids, \ 
-						minescape.uuids \
-						WHERE cache.hiscores_normal_exp.id = minescape.character_ids.id \
-						AND minescape.character_ids.uuid = minescape.uuids.uuid \
-						ORDER BY cache.hiscores_normal_exp.${skill} \
-						DESC LIMIT ? OFFSET ?;`;
-	queryCall(selectSql, [perPage, offset], (error, results) => {
-		if(error || !results.length){
-			console.error(error);
-			return interaction.editReply({ content: 'No results found.'});
-		}
-		handleResult(interaction, skill, results, offset);
-	});
+	const queryCallback = async (interaction: ChatInputCommandInteraction<CacheType> | ButtonInteraction, page: number) => {
+		const offset = (page - 1) * perPage;
+	
+		const selectSql = `SELECT cache.hiscores_normal_exp.${skill}, \
+							minescape.character_ids.uuid, \
+							minescape.uuids.username FROM cache.hiscores_normal_exp, \
+							minescape.character_ids, \ 
+							minescape.uuids \
+							WHERE cache.hiscores_normal_exp.id = minescape.character_ids.id \
+							AND minescape.character_ids.uuid = minescape.uuids.uuid \
+							ORDER BY cache.hiscores_normal_exp.${skill} \
+							DESC LIMIT ? OFFSET ?;`;
+		queryCall(selectSql, [perPage, offset], (error, results) => {
+			if(error || !results.length){
+				console.error(error);
+				if((interaction as ButtonInteraction<CacheType>).update){
+					return (interaction as ButtonInteraction<CacheType>).update({ content: 'No results found.'});
+				}
+				return interaction.editReply({ content: 'No results found.'});
+			}
+			handleResult(interaction, skill, results, offset, page, queryCallback);
+		});
+	}
+	queryCallback(interaction, page);
 }
 
 
-async function handleResult(interaction: ChatInputCommandInteraction<CacheType>, type: string, results: any, offset: number) {
+async function handleResult(interaction: ChatInputCommandInteraction<CacheType> | ButtonInteraction, type: string, results: any, offset: number, page: number, queryCallback: (interaction: ChatInputCommandInteraction<CacheType> | ButtonInteraction, page: number) => void) {
 	const emoji = emojis[type as keyof typeof emojis];
 	const embed = new EmbedBuilder()
 					.setTitle(`Top Players: ${capitalize(type)} ${emoji}`)
@@ -162,7 +185,42 @@ async function handleResult(interaction: ChatInputCommandInteraction<CacheType>,
 		const number = index + 1 + offset;
 		embed.addFields({name: `${number}. \`\`\`${result.username}\`\`\``, value: `${emoji} Level: ${level}\n[${formatNumber(result[val])}]`, inline: true});
 	});
-	interaction.editReply({ embeds: [embed] });
+
+    const row = new ActionRowBuilder<ButtonBuilder>().addComponents(new ButtonBuilder().setCustomId(`previous-${page - 1}`).setLabel('Previous').setStyle(ButtonStyle.Danger).setDisabled(page === 1), new ButtonBuilder().setCustomId(`next-${page + 1}`).setLabel('Next').setStyle(ButtonStyle.Success));
+
+	if((interaction as ButtonInteraction<CacheType>).update){
+		await (interaction as ButtonInteraction<CacheType>).update({ content: '', embeds: [embed], components: [row] });
+	}else{
+		await interaction.editReply({ content: '', embeds: [embed], components: [row] });
+		const message = await interaction.fetchReply();
+	
+		const collector = message.createMessageComponentCollector({ componentType: ComponentType.Button, time: 100 });
+
+		collector.on('collect', async (buttonInteraction: ButtonInteraction<CacheType>) => {
+			if (buttonInteraction.user.id !== interaction.user.id) {
+				interaction.reply({ content: 'These buttons aren\'t for you!', ephemeral: true });
+				return
+			}
+
+			const [action, inputPage] = buttonInteraction.customId.split('-');
+			//validate inputPage
+			const page = parseInt(inputPage);
+
+			if (action === 'next' && page >= 1) {
+				// await buttonInteraction.deferUpdate();
+				queryCallback(buttonInteraction, page)
+			} else if (action === 'previous' && page >= 1) {
+				// await buttonInteraction.deferUpdate();
+				queryCallback(buttonInteraction, page)
+			}
+
+		});
+
+		collector.on('end', async () => {
+			interaction.editReply({ components: [] });
+		})
+	}
+	
 }
 
 
